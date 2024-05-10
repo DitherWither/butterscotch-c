f2feabda2a2ed55be3142dcae51f906f77d400ea
Saumya Patil <saumyasachinpatil@gmail.com>
2024-05-10 18:53:31 +0530

    Initial Commit

---
+++ iso.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+. ./build.sh
+
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp sysroot/boot/butterscotch.kernel isodir/boot/butterscotch.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "butterscotch" {
+	multiboot /boot/butterscotch.kernel
+}
+EOF
+grub2-mkrescue -o butterscotch.iso isodir
